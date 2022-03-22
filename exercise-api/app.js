6841d13f5621a203a2104c979df856d368f8b7d0
Slice913 <akeemayoade@gmail.com>
2022-03-22 13:00:32 -0500

    created get route to get all data

--- exercise-api/app.js
+++ exercise-api/app.js
@@ -1,7 +1,9 @@
-const express = require('express')
+const express = require('express');
+const { send } = require('express/lib/response');
 const app = express()
 const port = 3000
 const mongoose = require('mongoose');
+const { db } = require('./api/models/store');
 const Store = require('./api/models/store');
 
 mongoose.connect('mongodb+srv://Slicefpv:Spiderman2w3@cluster0.vmsuk.mongodb.net/?retryWrites=true&w=majority');
@@ -10,22 +12,56 @@ app.use(express.json({limit: '50mb'}));
 
  
 app.post('/api/stores', (req, res) => {
-  let dbstores = req.body;
+  let dbstores = [];
+  let stores = req.body;
+  stores.forEach((store)=>{
+    dbstores.push({
+      storeName: store.name, 
+      phoneNumber: store.phoneNumber,
+      address: store.address,
+      openStatusText: store.openStatusText,
+      addressLines: store.addressLines, 
+      location: {
+        type: "Point",
+          "coordinates" : [
+             store.coordinates.longitude,
+             store.coordinates.latitude
+          ]
+        }
+     })
+  })
    
-  res.send('Another endpoint Yeah!');
-})
+  Store.create(dbstores, (err, stores)=>{
+     if(err){
+      res.status(500).send(err);
+     } else {
+      res.status(200).send(stores);
+     }
+   })
+ });
+
+
 
 app.delete('/api/stores',  (req, res) => {
    Store.deleteMany({}, (err) => {
       res.status(200).send(err);
    })
-   
 })
 
+
 // Spiderman2w3 MongoDB
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/api/stores', (req, res) =>{
+  Store.find({}, (err, stores)=>{
+   if(err){
+     res.status(500).send(err);
+    } else {
+      res.status(200).send(stores);
+    }
+  })
 })
+  
+
+
 
 app.listen(port, () => {
   console.log(`Google Store Locator 🌏  ${port}`)
